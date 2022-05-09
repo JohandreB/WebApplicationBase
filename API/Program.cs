8fa57f16b48d046cf7e1f2e5f27a20f0e1a6ae65
JohandreB <2017126596@ufs4life.ac.za>
2022-05-10 00:55:25 +0200

    Basic API structure created

---
+++ API/Program.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. Section is replacement for previous ConfigureServices method
+ConfigurationManager config = builder.Configuration;
+builder.Services.AddDbContext<DataContext>(options=>options.UseSqlite(config.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. Replacement for the Configure method
+if (app.Environment.IsDevelopment())
+{
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
